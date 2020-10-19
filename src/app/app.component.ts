05ca95a613d12db8129f3a0d89a419d966894e6e
Jeroen Klein Geltink <jeroen_klein_geltink@hotmail.com>
2020-10-19 23:08:22 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShoppingItem } from './store/models/shopping-item.model';
+import { AppState } from './store/models/app-state.model';
+import { Store } from '@ngrx/store';
+import {
+  AddItemAction,
+  DeleteItemAction,
+  LoadShoppingAction,
+} from './store/actions/shopping.actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ngrx-test';
+export class AppComponent implements OnInit {
+  shoppingItems: Observable<Array<ShoppingItem>>;
+  loading$: Observable<Boolean>;
+  error$: Observable<Error>;
+  newShoppingItem: ShoppingItem = { id: '', name: '' };
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit() {
+    this.shoppingItems = this.store.select((store) => store.shopping.list);
+    this.loading$ = this.store.select((store) => store.shopping.loading);
+    this.error$ = this.store.select((store) => store.shopping.error);
+
+    this.store.dispatch(new LoadShoppingAction());
+  }
+
+  addItem() {
+    this.newShoppingItem.id = Math.floor(Math.random() * 1000 + 1).toString();
+
+    this.store.dispatch(new AddItemAction(this.newShoppingItem));
+
+    this.newShoppingItem = { id: '', name: '' };
+  }
+
+  deleteItem(id: string) {
+    this.store.dispatch(new DeleteItemAction(id));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule } from '@angular/forms';
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import { ShoppingEffects } from './store/effects/shopping.effects';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({ shopping: ShoppingReducer }),
+    EffectsModule.forRoot([ShoppingEffects]),
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
